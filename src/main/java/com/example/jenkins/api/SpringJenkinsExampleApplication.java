1be33cce388d47062982ef8a5b315105cc64a4ac
java9 <rajeshsheelam19@gmail.com>
2024-02-18 01:13:33 +0530

    files added

---
+++ src/main/java/com/example/jenkins/api/SpringJenkinsExampleApplication.java
@@ -0,0 +1,24 @@
+package com.example.jenkins.api;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import jakarta.annotation.PostConstruct;
+
+@SpringBootApplication
+public class SpringJenkinsExampleApplication {
+
+	static Logger logger = LoggerFactory.getLogger(SpringJenkinsExampleApplication.class);
+	
+	@PostConstruct
+	public void init() {
+		logger.info("Application started");
+	}
+	public static void main(String[] args) {
+		logger.info("Application executed");
+		SpringApplication.run(SpringJenkinsExampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/jenkins/api/SpringJenkinsExampleApplicationTests.java
@@ -0,0 +1,18 @@
+package com.example.jenkins.api;
+
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJenkinsExampleApplicationTests {
+
+	static Logger logger = LoggerFactory.getLogger(SpringJenkinsExampleApplicationTests.class);
+	
+	@Test
+	void contextLoads() {
+		logger.info("Test case executing");
+	}
+
+}
